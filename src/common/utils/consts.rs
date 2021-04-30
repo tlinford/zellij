b12d046262f9792777d6b04f102e7df0768f3d17
Kunal Mohan <kunalmohan99@gmail.com>
2021-04-30 20:22:09 +0530

    Place socket file in runtime directory or cache directory and use names crate for socket file name

--- src/common/utils/consts.rs
+++ src/common/utils/consts.rs
@@ -3,4 +3,3 @@
 pub const ZELLIJ_TMP_DIR: &str = "/tmp/zellij";
 pub const ZELLIJ_TMP_LOG_DIR: &str = "/tmp/zellij/zellij-log";
 pub const ZELLIJ_TMP_LOG_FILE: &str = "/tmp/zellij/zellij-log/log.txt";
-pub const ZELLIJ_IPC_PIPE: &str = "/tmp/zellij/ipc";
